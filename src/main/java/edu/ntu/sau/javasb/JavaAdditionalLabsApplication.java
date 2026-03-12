b209bba4768a1d9e539b276aac054011de825679
ominieiev <ominieiev@gmail.com>
2026-03-12 15:13:16 +0200

    PBC-12456:Create initial app

---
+++ src/main/java/edu/ntu/sau/javasb/JavaAdditionalLabsApplication.java
@@ -0,0 +1,16 @@
+package edu.ntu.sau.javasb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class JavaAdditionalLabsApplication {
+
+    public static void main(String[] args) {
+
+        ConfigurableApplicationContext context = SpringApplication.run(JavaAdditionalLabsApplication.class, args);
+
+    }
+
+}
---
+++ src/main/java/edu/ntu/sau/javasb/controllers/MainController.java
@@ -0,0 +1,26 @@
+package edu.ntu.sau.javasb.controllers;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+@Controller
+public class MainController {
+
+
+
+    @GetMapping("/welcome")
+    String getWelcome(Model model) {
+
+        return "welc";
+    }
+
+}
